8ba80c29b4f793a489ae232c7b4a7bd0f64d3266
johnwilsoniv <johnwilsoniv@gmail.com>
2025-12-17 12:15:44 -0500

    Sync version to v1.3.3 in both config files

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pyfaceau",
-    version="1.3.2",
+    version="1.3.3",
     author="John Wilson",
     author_email="",  # Add email if desired
     description="Pure Python OpenFace 2.2 AU extraction with PyMTCNN face detection and CLNF refinement",
